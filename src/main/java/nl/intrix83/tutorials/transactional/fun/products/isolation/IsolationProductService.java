d76c269e23834f1276e6575cde6a71dd9fe79034
Koehler, A (Arno) <arno.koehler@rabobank.nl>
2020-04-27 22:29:31 +0200

    isolation and read only

--- src/main/java/nl/intrix83/tutorials/transactional/fun/products/isolation/IsolationProductService.java
+++ src/main/java/nl/intrix83/tutorials/transactional/fun/products/isolation/IsolationProductService.java
@@ -1,5 +1,7 @@
 package nl.intrix83.tutorials.transactional.fun.products.isolation;
 
+import java.util.List;
+
 import nl.intrix83.tutorials.transactional.fun.products.Product;
 import nl.intrix83.tutorials.transactional.fun.products.ProductRepository;
 
@@ -28,12 +30,30 @@ public class IsolationProductService {
      * T2 ------------10 --------------> 10 ????
      **/
     @Transactional(isolation = Isolation.READ_COMMITTED)
-    public void addTenProducts() {
-        for (int i = 0; i < 10; i++) {
+    public void addHundredProducts() {
+        for (int i = 0; i < 100; i++) {
             productRepository.save(new Product(null, "Beer " + i));
-            if (i == 3) {
-                throw new RuntimeException("Third of ten");
-            }
         }
+    }
+
+    /**
+     * readOnly = true
+     *
+     * only for pure gets this might be fast because the way it's handled (without transaction overhead)
+     *
+     * NOTE: A transaction manager which cannot interpret the read-only hint wil
+     * not throw an exception when asked for a read-only transaction
+     *
+     * This might vary over different database implementations
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<Product> getAllFast() {
+       return productRepository.findAll();
+    }
+
+    @Transactional
+    public List<Product> getAll() {
+        return productRepository.findAll();
     }
 }
---
+++ src/test/java/nl/intrix83/tutorials/transactional/fun/products/isolation/IsolationProductServiceTest.java
@@ -0,0 +1,54 @@
+package nl.intrix83.tutorials.transactional.fun.products.isolation;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import nl.intrix83.tutorials.transactional.fun.products.Product;
+import nl.intrix83.tutorials.transactional.fun.products.ProductRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class IsolationProductServiceTest {
+
+    @Autowired
+    private IsolationProductService isolationProductService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Before
+    public void before() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldCommit() {
+        isolationProductService.addHundredProducts();
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            List<Product> allFast = isolationProductService.getAllFast();
+            assert allFast.size() == 100;
+        }
+        long end = System.currentTimeMillis();
+        long timeReadOnly = end - start;
+
+
+        long normalStart = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            List<Product> allFast = isolationProductService.getAll();
+            assert allFast.size() == 100;
+        }
+        long normalEnd = System.currentTimeMillis();
+        long timeNormal = normalEnd - normalStart;
+
+        assertThat(timeReadOnly).isLessThan(timeNormal);
+    }
+}
